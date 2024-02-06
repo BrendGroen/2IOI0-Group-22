eeddb0711e343d31c25caf7a39d6d737e3cf1c3d
Boorunit <39187637+BoschAccuBoormachine@users.noreply.github.com>
2024-02-06 11:46:56 +0100

    add df

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pm4py
-import os
-os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+import pandas as pd
 
 log = pm4py.read_xes('BPI_Challenge_2012.xes')
 process_model = pm4py.discover_bpmn_inductive(log)
 pm4py.view_bpmn(process_model)
+df = pm4py.convert_to_dataframe(log)
