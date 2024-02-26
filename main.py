93c4b29c26771d2bbafb1791f7f546d4afc48a84
20211576 <k.gheytuli@student.tue.nl>
2024-02-26 20:21:16 +0100

    deleted the main.py file

--- main.py
+++
@@ -1,8 +0,0 @@
-import pm4py
-import pandas as pd
-
-log = pm4py.read_xes('BPI_Challenge_2012.xes')
-# process_model = pm4py.discover_bpmn_inductive(log)
-# pm4py.view_bpmn(process_model)
-df = pm4py.convert_to_dataframe(log)
-print(df.columns)
